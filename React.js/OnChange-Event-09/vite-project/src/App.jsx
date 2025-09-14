710bc55945083bf3066e4ebeb5c1269efa98b8a8
Mindgame786 <umarjutt111jb@gmail.com>
2025-09-14 11:39:53 +0500

    Added MyProject folder

---
+++ React.js/OnChange-Event-09/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-sans font-bold bg-zinc-50 text-pink-400 rounded-md text-shadow-sm text-shadow-zinc-600  inset-shadow-zinc-300 inset-shadow-sm py-4'>
+        React + Vite + Tailwind
+      </h1>
+
+    </>
+  )
+}
+
+export default App;
