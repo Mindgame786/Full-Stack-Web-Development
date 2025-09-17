2c64fe22efad5afe4ac105da1997c4d4b01aa525
Mindgame786 <umarjutt111jb@gmail.com>
2025-09-17 13:26:57 +0500

    Update form + live preview + toast notifications

---
+++ React.js/Controlled-Components-10/vite-project/src/index.js
@@ -0,0 +1,13 @@
+//---------------------------------------------------------------------------------------------                              Controlled Component in React                                  ---------------------------------------------------------------------------------------------
+
+// A Controlled Component is  an input element whose value is managed by the React state using the useState() hook (or class components state).
+
+
+//  Benefits of  controlled components?
+
+// Better control over form data
+// Easy validation (Before Submit)
+// One source of truth (React state)
+// Dynamic rendering based on user input (Due to state)
+// Value attribute is attached with state.
+
