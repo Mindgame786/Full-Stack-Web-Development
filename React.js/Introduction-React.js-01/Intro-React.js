964f35849d13cb67cbadd37282c6a819bdbffe76
Mindgame786 <umarjutt111jb@gmail.com>
2025-11-04 11:12:05 +0500

    Updated project code

--- React.js/Introduction-React.js-01/Intro-React.js
+++ React.js/Introduction-React.js-01/Intro-React.js
@@ -1,26 +1,38 @@
 //--------------------------------React-JS--------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-//====================================INTRODUCTION===========================================
+//=========================================== INTRODUCTION ===============================================
 
-// React JS  is  a free  and  opern-source JavaScript library used to build  user (UI) interfaces for web applications.It was released in 2013.
+//  (1) React JS  is  a free  and  opern-source JavaScript library used to build  user (UI) interfaces for web applications.It was released in 2013.
 
 
-
-//  It helps developers create fast and  interactive websites  and apps.
+//  (2) It helps developers create fast and  interactive websites  and apps.
 //  Made by Facebook --  React was created by Facebook and is used in many big websites like Facebook and  Instagram.
 
-// Reusable Components ---React lets  you build parts of  a  website  (called components) like  buttons, menus, or forms that you can reuse in differents place.
 
-//===========================================================================================
-
-// Fast Updates --  When something changes  on the  web page it updates only that part instead of  reloading the whole page.This makes it very fast.Its is also called Single Page Application.(SPA).
+// (3) Reusable Components ---React lets  you build parts of  a  website  (called components) like  buttons, menus, or forms that you can reuse in differents place.
 
 
-// Easy to Manage -- React helps in  organizing  code and makes it easier to manage Big Projects.
+//========================================================================================================
 
 
-// Used With JavaScript  -- React works mainly with JavaScript, so If  You know JavaScript, You can Learn React Easily.
+// ---------------------------------------Reasons Why Used React.js---------------------------------------
+
+
+// (1)  Fast Updates --  When something changes  on the  web page it updates only that part instead of  reloading the whole page.This makes it very fast.Its is also called Single Page Application.(SPA).Example punjab univeristy web page which (SPA).
+
+
+// (2)  Easy to Manage -- React helps in  organizing  code and makes it easier to manage Big Projects.
+
+
+// (3)  Used With JavaScript  -- React works mainly with JavaScript, so If  You know JavaScript, You can Learn React Easily.
+
+
+//  (4)  Made By Most Famous App Facebook, Instagram, and Netflix used in React.JS.
+
+
+
+//----------------------------------------------------------------------------------------------------------
 
 
 
